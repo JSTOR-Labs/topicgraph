e66b3f976f7963a8c4187fea35a2864e38763db3
rsnyder <ron@snyderjr.com>
2017-05-31 07:07:55 -0400

    Use latest topic model (llda-20170524) for document analysis.

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -2,4 +2,4 @@ export const MOBILE = (typeof window !== 'undefined') ? (window.screen.availWidt
 // export const API_BASE_URL: string = 'http://localhost:8000';
 export const API_BASE_URL: string = 'https://labs.jstor.org';
 export const SERVICES_HOST: string = 'http://localhost:8080/';
-export const TOPIC_MODEL: string = 'llda-20170319';
+export const TOPIC_MODEL: string = 'llda-20170524';
